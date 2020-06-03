c356fed80884316fd70b78ba3386117b8d2510f7
SaberNero <pishniy.s@gmail.com>
2020-06-03 15:35:13 +0300

    Added a test program for detecting devices

---
+++ TestConnectedUSBDevices/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace TestConnectedUSBDevices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usbDevices = GetUSBDevices();
+
+            foreach (var usbDevice in usbDevices)
+            {
+                Console.WriteLine("Description: {2}",
+                    usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+            }
+
+            Console.Read();
+
+        }
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            //using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+            using (var searcher = new ManagementObjectSearcher("root\\CIMV2",
+            @"SELECT * FROM Win32_PnPEntity where DeviceID Like ""USB%"""))
+                collection = searcher.Get();
+            foreach (var device in collection)
+            {
+                foreach (var item2 in device.Properties)
+                {
+                    Console.WriteLine(item2.Name+":  "+item2.Value);
+                }
+            }
+            Console.ReadLine();
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    }
+
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
